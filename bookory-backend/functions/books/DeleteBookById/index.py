3328f83f0070908770af5ca7b1df857fc395edb1
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2026-03-20 18:31:49 +0100

    delete book by id done and working

---
+++ bookory-backend/functions/books/DeleteBookById/index.py
@@ -0,0 +1,23 @@
+from responses.index import send_response
+from middlewares.errorHandler import error_handler
+from middlewares.authenticateUser import authenticate_user
+from services.books import delete_book_by_id
+
+@error_handler
+@authenticate_user
+
+def handler(event, context):
+  path = event.get("pathParameters") or {}
+  book_id = path.get("id")
+  user = event["user"]
+  userid = user["userid"]
+
+  if not book_id:
+    return send_response(400, {"message": "Missing book id"})
+  
+  result = delete_book_by_id(userid, book_id)
+
+  if result:
+    return send_response(201, result)
+  else:
+    return send_response(500, {"message": "Error deleting book"})
--- bookory-backend/services/books.py
+++ bookory-backend/services/books.py
@@ -119,3 +119,20 @@ def get_book_by_id(user_id, book_id):
   except Exception as error:
     print("Error getting book:", error)
     return None
+  
+def delete_book_by_id(user_id, book_id):
+  try:
+    table.delete_item(
+      Key={
+        "PK": f"USER#{user_id}",
+        "SK": f"BOOK#{book_id}"
+      }
+    )
+    return {
+      "success": True,
+      "bookId": book_id,
+    }
+
+  except Exception as error:
+    print("Error deleting book:", error)
+    return False
